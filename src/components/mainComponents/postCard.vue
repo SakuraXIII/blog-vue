6ebe020c7f4509c6fc18d853086fa3f65e3d2d11
SakuraXIII <1162617171@qq.com>
2020-04-04 20:19:39 +0800

    增加未获取到数据时的占位提示

--- src/components/mainComponents/postCard.vue
+++ src/components/mainComponents/postCard.vue
@@ -10,9 +10,11 @@
           :wordNum="item.article ? item.article.length : ''"
           :read-time="item.time"
           :tags="'折腾'"
+          v-if="postlist !== '正在加载中'"
         />
+        <p v-else class="loading">(。・∀・)ノ)数据好像还没收到，刷新下吧 <i class="el-icon-loading"></i></p>
         <div class="text">
-                  {{ item.article ? item.article.slice(0,200) + '...' : '' }}
+          {{ item.article ? item.article.slice(0,200) + '...' : '' }}
         </div>
       </div>
     </div>
@@ -40,10 +42,14 @@ export default {
     border-radius: var(--borRadius);
 
     .post-card {
-      padding: 1vw;
+      padding: 30px;
+      .loading {
+        text-align: center;
+      }
       .text {
         margin-top: 1vw;
         text-indent: 2em;
+        line-height: 1.5em;
       }
     }
   }
